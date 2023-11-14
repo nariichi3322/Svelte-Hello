204f015cfa76c6db79c3695c89cc359ad486255d
Nariichi Ishimine <nariichi3322@gmail.com>
2023-11-14 09:48:29 +0800

    feat: remove pages with server requirements

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-    import { base } from '$app/paths';
-</script>
-
 <svelte:head>
     <title>About</title>
     <meta name="description" content="About this app" />
@@ -21,10 +17,5 @@
         The page you're looking at is purely static HTML, with no client-side interactivity needed.
         Because of that, we don't need to load any JavaScript. Try viewing the page's source, or
         opening the devtools network panel and reloading.
-    </p>
-
-    <p>
-        The <a href="{base}/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling.
-        Try using it with JavaScript disabled!
     </p>
 </div>
